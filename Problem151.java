3c27e8f76b08271aa55d0fae230bb6be2dbc13bf
saikam2003 <126769938+saikam2003@users.noreply.github.com>
2024-07-16 23:30:07 +1200

    Complete problem 151

--- Problem151.java
+++ Problem151.java
@@ -1,10 +1,37 @@
-public class Problem151 {
-    public String reverseWords(String s) {
-        if (s.length() == 1) {
-            return s;
-        }
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class Problem151 {
+
+    public static void main(String[] args) {
+        // Your code here
+        String result = reverseWords("  hello world  ");
+        System.out.println(result);
+    }
+    
+    public static String reverseWords(String s) {
+        List<String> str = new ArrayList<>(Arrays.asList(s.split(" ",-1)));
+        StringBuilder sb = new StringBuilder(); 
+        str.removeAll(Collections.singleton(""));
         
-        return s;
+        Collections.reverse(str);
+
+        int i = 0;
+        while (i < str.size()) {
+            // if (!str.get(i).equals("")) {
+                if (i == str.size() - 1) {
+                    sb.append(str.get(i));
+                } else {
+                sb.append(str.get(i));
+                sb.append(" ");
+            }
+            // }
+            
+        i++;
+        }
+        
+        return sb.toString();
     }
 }
