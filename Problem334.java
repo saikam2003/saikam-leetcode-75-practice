ba0f3a5bd71f733d68fbebe302a90b92c2f01f4a
saikam2003 <126769938+saikam2003@users.noreply.github.com>
2024-07-18 00:54:58 +1200

    trying to solve problem 334

--- Problem334.java
+++ Problem334.java
@@ -1,16 +1,59 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem334 {
 
     public static void main(String[] args) {
         // Your code here
-        System.out.println(increasingTriplet(new int[] {1,2,3,4,5}));
+        System.out.println(increasingTriplet(new int[] { 2,1,5,0,4,6 }));
     }
-    
+
     public static boolean increasingTriplet(int[] nums) {
-        List<Integer> numsList = Arrays.stream(nums).boxed().toList();
-        return numsList.stream().anyMatch(i -> (nums[i]<nums[i+1] && nums[i+1]<nums[i+2] && i<nums.length-3));
+        
+        List<Tuple<Integer, Integer>> numsList = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Tuple<Integer, Integer> temp = new Tuple<>(nums[i], i);
+            numsList.add(temp);
+        }
+
+        
+        Collections.sort(numsList, new Comparator<Tuple<Integer, Integer>>() {
+            @Override
+            public int compare(Tuple<Integer, Integer> t1, Tuple<Integer, Integer> t2) {
+                return t1.getFirst().compareTo(t2.getFirst());
+            }
+        });
+
+        for (int i = 0; i < numsList.size() - 2; i++) {
+            if ((numsList.get(i).getFirst() < numsList.get(i+1).getFirst() && numsList.get(i+1).getFirst() < numsList.get(i+2).getFirst()) && (numsList.get(i).getSecond() < numsList.get(i+1).getSecond() && numsList.get(i+1).getSecond() < numsList.get(i+2).getSecond())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+class Tuple<A, B> {
+
+    private final A first;
+    private final B second;
+
+    public Tuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
     }
 }
