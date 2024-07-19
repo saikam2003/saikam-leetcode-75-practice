d1e8230158c3c3586a9ddb3c19727223d23dd78f
saikam2003 <126769938+saikam2003@users.noreply.github.com>
2024-07-20 01:35:40 +1200

    Completed problem 334 (referred to solution)

--- Problem334.java
+++ Problem334.java
@@ -13,47 +13,18 @@ public class Problem334 {
     }
 
     public static boolean increasingTriplet(int[] nums) {
-        
-        List<Tuple<Integer, Integer>> numsList = new ArrayList<>();
+        int i = Integer.MAX_VALUE;
+        int j = Integer.MAX_VALUE;
 
-        for (int i = 0; i < nums.length; i++) {
-            Tuple<Integer, Integer> temp = new Tuple<>(nums[i], i);
-            numsList.add(temp);
-        }
-
-        
-        Collections.sort(numsList, new Comparator<Tuple<Integer, Integer>>() {
-            @Override
-            public int compare(Tuple<Integer, Integer> t1, Tuple<Integer, Integer> t2) {
-                return t1.getFirst().compareTo(t2.getFirst());
-            }
-        });
-
-        for (int i = 0; i < numsList.size() - 2; i++) {
-            if ((numsList.get(i).getFirst() < numsList.get(i+1).getFirst() && numsList.get(i+1).getFirst() < numsList.get(i+2).getFirst()) && (numsList.get(i).getSecond() < numsList.get(i+1).getSecond() && numsList.get(i+1).getSecond() < numsList.get(i+2).getSecond())) {
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] < i) {
+                i = nums[k];
+            } else if (nums[k] > i && nums[k] < j) {
+                j = nums[k];
+            } else if (nums[k] > j) {
                 return true;
-            }
+            } 
         }
-
         return false;
-    }
-}
-
-class Tuple<A, B> {
-
-    private final A first;
-    private final B second;
-
-    public Tuple(A first, B second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public A getFirst() {
-        return first;
-    }
-
-    public B getSecond() {
-        return second;
     }
 }
