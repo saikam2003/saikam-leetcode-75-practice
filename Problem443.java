d6d4a84792a972c376a1b7a03346eac37213d2be
saikam2003 <126769938+saikam2003@users.noreply.github.com>
2024-07-20 14:55:57 +1200

    Solved problem 443 with O(n) time complexity

---
+++ Problem443.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Problem443 {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println(compress(new char[] {'a','a','a','b','b','a','a'}));
+    }
+
+    public static int compress(char[] chars) {
+        String str = "";
+        int i = 0;
+        int currentCount = 0;
+        while (i < chars.length) {
+            if (str.contains(Character.toString(chars[i]))) {
+                if (chars[i - 1] != chars[i] && i > 1) {
+                    currentCount = 0;
+                    str = str.concat(Character.toString(chars[i]));
+                    currentCount++;
+                } else {
+                    currentCount++;
+                    if ((i < chars.length - 1) && chars[i + 1] != chars[i] && currentCount != 1) {
+                        str = str.concat(Integer.toString(currentCount));
+                        currentCount = 0;
+                    } else if (i == chars.length - 1) {
+                        str = str.concat(Integer.toString(currentCount));
+                    }
+                }
+                
+            } else {
+                currentCount = 0;
+                str = str.concat(Character.toString(chars[i]));
+                currentCount++;
+            }
+
+            i++;
+        }
+        
+        System.arraycopy(str.toCharArray(), 0, chars, 0, str.length());
+        return str.length();    
+    }
+}
